9b2974317294517c2791ee76cadf0ddb3a886877
Pascal Thomet <pthomet@gmail.com>
2022-04-15 01:32:31 +0200

    Correct segfault in gl_provider_python

--- src_all_in_one/immvision/immvision.cpp
+++ src_all_in_one/immvision/immvision.cpp
@@ -4606,18 +4606,32 @@ namespace pybind11
 //                       src/immvision/internal/gl_provider_python.cpp continued                                //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
 
 
 namespace ImmVision_GlProvider
 {
-    namespace PythonFunctions
+    struct PythonFunctions
     {
-        pybind11::object python_module = pybind11::module_::import("gl_provider_python");
-        pybind11::object Blit_RGBA_Buffer = python_module.attr("Blit_RGBA_Buffer");
-        pybind11::object GenTexture = python_module.attr("GenTexture");
-        pybind11::object DeleteTexture = python_module.attr("DeleteTexture");
-    }
+        PythonFunctions()
+        {
+            PythonModule = pybind11::module_::import("gl_provider_python");
+            Blit_RGBA_Buffer = PythonModule.attr("Blit_RGBA_Buffer");
+            GenTexture = PythonModule.attr("GenTexture");
+            DeleteTexture = PythonModule.attr("DeleteTexture");
+        }
+
+        pybind11::object PythonModule;
+        pybind11::object Blit_RGBA_Buffer;
+        pybind11::object GenTexture;
+        pybind11::object DeleteTexture;
+
+        static PythonFunctions Instance()
+        {
+            static PythonFunctions instance;
+            return instance;
+        }
+    };
+
 
     void Blit_RGBA_Buffer(unsigned char *image_data, int image_width, int image_height, unsigned int textureId)
     {
@@ -4625,17 +4639,21 @@ namespace ImmVision_GlProvider
 //        pybind11::object python_module = pybind11::module_::import("gl_provider_python");
 //        (void)python_module;
 
+        //PythonFunctions gPythonFunctions;
+
         std::cout << "C++ : Blit_RGBA_Buffer about to create Mat\n";
         cv::Mat m(image_height, image_width, CV_8UC4, image_data);
         std::cout << "C++ : Blit_RGBA_Buffer about to call PythonFunctions::Blit_RGBA_Buffer\n";
-        PythonFunctions::Blit_RGBA_Buffer(m, textureId);
+        PythonFunctions::Instance().Blit_RGBA_Buffer(m, textureId);
         std::cout << "C++ : Blit_RGBA_Buffer done!\n";
     }
 
     unsigned int GenTexture()
     {
+        //PythonFunctions gPythonFunctions;
+
         std::cout << "C++ About to call GenTexture\n";
-        pybind11::object id_object = PythonFunctions::GenTexture();
+        pybind11::object id_object = PythonFunctions::Instance().GenTexture();
         auto texture_id = id_object.cast<unsigned int>();
         std::cout << "C++ After calling GenTexture, texture_id=%i" << texture_id << "\n";
         return  texture_id;
@@ -4643,8 +4661,10 @@ namespace ImmVision_GlProvider
 
     void DeleteTexture(unsigned int texture_id)
     {
+        //PythonFunctions gPythonFunctions;
+
         std::cout << "C++ About to call DeleteTexture\n";
-        PythonFunctions::DeleteTexture(texture_id);
+        PythonFunctions::Instance().DeleteTexture(texture_id);
         std::cout << "C++ After calling DeleteTexture\n";
     }
 }
