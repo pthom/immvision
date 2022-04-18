a22b62e26bea8b3efd264d182cb23054b4694359
Pascal Thomet <pthomet@gmail.com>
2022-04-19 00:38:31 +0200

    add test.py

---
+++ immvision_pybind/src/immvision/test.py
@@ -0,0 +1,7 @@
+print("In test immvision")
+import imgui
+import cv2
+from .cpp_immvision import __doc__, __version__
+from .cpp_immvision import Image, ImageNavigator
+from .cpp_immvision import InitGlProvider, ResetGlProvider
+
