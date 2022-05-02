27699b7681410dcf8c89a2f5f31aab5e3977c145
Pascal Thomet <pthomet@gmail.com>
2022-05-02 16:47:55 +0200

    backend_factory.py: imports shall not be done at top level

--- pybind/src_python/immvision/imgui_runner/backend_factory.py
+++ pybind/src_python/immvision/imgui_runner/backend_factory.py
@@ -1,7 +1,5 @@
 from enum import Enum
 from .any_backend import AnyBackend
-from .sdl_backend import SdlBackend
-from .glfw_backend import GlfwBackend
 from .imgui_app_params import _ImguiAppParamsHelper
 
 
@@ -12,8 +10,10 @@ class BackendType(Enum):
 
 def factor_backend(backend_type: BackendType, imgui_app_params_helper: _ImguiAppParamsHelper) -> AnyBackend:
     if backend_type == BackendType.SDL:
-        return SdlBackend(imgui_app_params_helper)
+        from .sdl_backend import SdlBackend         # this import shall not be done at top level
+        return SdlBackend(imgui_app_params_helper)  # we want to run even if sdl is not installed, but glfw is
     elif backend_type == BackendType.GLFW:
+        from .glfw_backend import GlfwBackend       # this import shall not be done at top level
         return GlfwBackend(imgui_app_params_helper)
     else:
         raise RuntimeError("factor_backend: unknown backend")
