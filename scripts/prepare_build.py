a8884dcbfca57954e84dea653518562cf7f07b03
Pascal Thomet <pthomet@gmail.com>
2022-11-15 13:44:59 +0100

    Review CI build

--- scripts/prepare_build.py
+++ scripts/prepare_build.py
@@ -7,25 +7,39 @@ import ci_utils
 
 def install_conan_if_needed():
     if not ci_utils.has_program("conan"):
-        ci_utils.run("pip3 install conan")
+        if platform.system() == "Darwin":
+            ci_utils.run("brew install conan")
+        else:
+            ci_utils.run("pip3 install conan")
     ci_utils.run("conan profile new default --detect --force")
     if platform.system() == "Linux":
         ci_utils.run("conan profile update settings.compiler.libcxx=libstdc++11 default")
 
 
-def prepare_build(build_folder="build"):
-    ci_utils.do_clone_repo("https://github.com/pthom/imgui_bundle.git", "imgui_bundle", pull=True, init_submodules=True)
-
+def _install_opencv_conan(build_folder):
     # Make build folder (absolute path)
     if not os.path.isabs(build_folder):
         build_folder = ci_utils.REPO_DIR + "/" + build_folder
     if not os.path.isdir(build_folder):
         os.makedirs(build_folder)
-
     # Run conan install
     install_conan_if_needed()
     ci_utils.chdir(build_folder)
-    ci_utils.run("conan install .. --build outdated")
+    ci_utils.run("conan install .. --build missing")
+
+
+def _install_opencv_package():
+    if platform.system() == "Linux":
+        ci_utils.run("brew install opencv")
+
+
+def prepare_build(build_folder="build", with_conan: bool = True):
+    ci_utils.do_clone_repo("https://github.com/pthom/imgui_bundle.git", "imgui_bundle", pull=True, init_submodules=True)
+    if with_conan:
+        _install_opencv_conan(build_folder)
+    else:
+        _install_opencv_package()
+
 
 
 if __name__ == "__main__":
