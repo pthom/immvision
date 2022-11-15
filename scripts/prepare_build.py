ab662e71a97d199cfecdc9db81a3b13b61a214e6
Pascal Thomet <pthomet@gmail.com>
2022-11-15 12:11:20 +0100

    Review CI

--- scripts/prepare_build.py
+++ scripts/prepare_build.py
@@ -9,6 +9,7 @@ import ci_utils
 def install_conan_if_needed():
     if not ci_utils.has_program("conan"):
         ci_utils.run("pip3 install conan")
+    ci_utils.run("conan profile update settings.compiler.libcxx=libstdc++11 default")
 
 
 def prepare_build(build_folder="build"):
@@ -23,7 +24,7 @@ def prepare_build(build_folder="build"):
     # Run conan install
     install_conan_if_needed()
     ci_utils.chdir(build_folder)
-    ci_utils.run("conan install .. --build outdated")
+    ci_utils.run("conan install .. --build outdated -c tools.system.package_manager:mode=install")
 
 
 if __name__ == "__main__":
