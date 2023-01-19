d5341de43d02a64869263711a8c45f08f716f700
Pascal Thomet <pthomet@gmail.com>
2023-01-19 18:40:47 +0100

    demo_immdebug.main.cpp: more realist demo

--- src/demos_immvision/demo_immdebug/demo_immdebug.main.cpp
+++ src/demos_immvision/demo_immdebug/demo_immdebug.main.cpp
@@ -1,59 +1,47 @@
 #include "immdebug/immdebug.h"
 
 #include <vector>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/core.hpp>
 
-
-cv::Mat MakeRandomImage()
+#include <filesystem>
+std::string ResourcesDir()
 {
-    cv::Size imageSize( rand() % 1200 + 10, rand() % 1000 + 10);
-
-    std::vector<int> imageTypes {
-        CV_8UC1,
-        CV_8UC3,
-        CV_8UC4,
-//        CV_32FC1,
-//        CV_64FC1
-    };
-    int imageType = imageTypes[rand() % imageTypes.size()];
-    cv::Mat m(imageSize, imageType);
-
-    double a0 = rand() % 10, a1 = rand() % 10, a2 = rand() % 10, a3 = rand() % 10, a4 = rand() % 10;
-    constexpr double pi = 3.141;
-
-    for (int row = 0; row < imageSize.height; ++row)
-    {
-        double y = (double) row / (double)imageSize.height * 2. * pi;
-        for (int col = 0; col < imageSize.width; ++col)
-        {
-            double x = (double) col / (double)imageSize.width * 2. * pi;
-
-            cv::Scalar color;
-
-            color[0] = cos(a0 * x) + sin(a1 * y) + cos(x * y * a3);
-            color[1] = sin(a0 * y) + sin(a1 * x) + cos(x * (y + 2.) * a4);
-            color[2] = cos(a0 * x) + sin(a2 * y) + cos(x * y * a1);
-            color[3] = cos(a0 * x) + sin(a1 * y) + cos(x * y * a0);
-
-            color = ((color / 3.) + cv::Scalar(1., 1., 1., 1.)) / 2.;
-            color[3] = 1.;
-
-            if ( (imageType == CV_8UC1) || (imageType == CV_8UC3) || (imageType == CV_8UC4))
-                color = color * 255.;
-
-            cv::Range xr(col, col + 1);
-            cv::Range yr(row, row + 1);
-            m(yr, xr) = color;
-        }
-    }
-    return m;
+    std::filesystem::path this_file(__FILE__);
+    return (this_file.parent_path().parent_path() / "resources").string();
 }
+
+
+void ExampleImageProcessingWithDebug()
+{
+    cv::Mat image = cv::imread(ResourcesDir() + "/house.jpg");
+    ImmVision::ImmDebug(image, "original");
+
+    cv::Mat roi = image(cv::Rect(800, 600, 400, 400));
+    ImmVision::ImmDebug(roi, "roi");
+
+    cv::Mat black;
+    cv::cvtColor(roi, black, cv::COLOR_RGB2GRAY);
+    ImmVision::ImmDebug(black, "black");
+
+    cv::Mat blur;
+    cv::blur(black, blur, cv::Size(5, 5));
+    ImmVision::ImmDebug(blur, "blur");
+
+    cv::Mat canny;
+    cv::Canny(blur, canny, 80., 120.);
+    ImmVision::ImmDebug(canny, "canny");
+
+    cv::Mat floatImage;
+    canny.convertTo(floatImage, CV_64FC1);
+    ImmVision::ImmDebug(floatImage, "floatImage");
+
+}
+
+
 
 int main()
 {
-    for (int i = 0; i < 10; ++i)
-    {
-        cv::Mat m = MakeRandomImage();
-        ImmVision::ImmDebug(m,"Rand image " + std::to_string(i));
-    }
+    ExampleImageProcessingWithDebug();
 }
