ab6c38569d8011a24b0e775d85d76eadac076a2a
Mark Harfouche <mark.harfouche@gmail.com>
2025-01-31 18:46:28 -0500

    Update image_drawing.cpp for compatibility with opencv 4.11
    See note in https://docs.opencv.org/4.11.0/d2/d44/classcv_1_1Rect__.html#a4506eca68c36d1c68af51489a7ac58a7

--- src/immvision/internal/drawing/image_drawing.cpp
+++ src/immvision/internal/drawing/image_drawing.cpp
@@ -106,7 +106,11 @@ namespace ImmVision
                         cv::Scalar white(255, 255, 255, 255);
                         cv::Scalar black(0, 0, 0, 255);
                         cv::Vec4b backgroundColor(0, 0, 0, 0);
-                        if ( cv::Rect(cv::Point(), drawingImage.size()).contains({(int)position.x, (int)position.y}))
+#if (CV_VERSION_MAJOR >= 5) || (CV_VERSION_MAJOR == 4 && CV_VERSION_MAJOR >= 11)
+                        if (cv::Rect(cv::Point(), drawingImage.size()).contains({position.x, position.y}))
+#else
+                        if (cv::Rect(cv::Point(), drawingImage.size()).contains({(int)position.x, (int)position.y}))
+#fi
                             backgroundColor = drawingImage.at<cv::Vec4b>((int)position.y, (int)position.x);
                         double luminance = backgroundColor[2] * 0.2126 + backgroundColor[1] * 0.7152 + backgroundColor[0] * 0.0722;
                         if (luminance > 170.)
